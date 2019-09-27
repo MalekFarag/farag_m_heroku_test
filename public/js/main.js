99fb69d5cf180dc9da678121dbf690fca1c15591
Malek F <43182938+MalekFarag@users.noreply.github.com>
2019-09-27 19:16:33 -0400

    all pages complete

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,14 @@
 (() => {
     console.log('Fired.');
 
+
+    // Nav functions
+
     const burger = document.querySelector('.burger'),
           nav    = document.querySelector('.mainNav');
 
     function openNav(){
+        burger.classList.toggle('rotate');
         nav.classList.toggle('open');
         console.log('Nav toggled.');
     }
