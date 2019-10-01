83a658cee928f0e923c7c58e394d29f48e369f36
Malek F <43182938+MalekFarag@users.noreply.github.com>
2019-10-01 16:00:05 -0400

    now using hbs to render pages (added var to page title)

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const hbs = require('hbs');
 const path = require('path'); // path lets us navigate file system / folders
 
 // heroku assigns a port wheni it deploys via the process (environment variables - comin)
@@ -8,20 +9,24 @@ const port = process.env.PORT || 3000; // a double pipe means "or"
 const app = express();
 
 app.use(express.static('public'));
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
   console.log('Home page');
-  res.sendFile(path.join(__dirname + '/views/index.html'));
-  // this builds localhost:3000/views/index.html
+  //res.sendFile(path.join(__dirname + '/views/index.html'));
+
+  res.render('home', {title: "Home"});
 })
 
 app.get('/merch', (req, res) => {
   console.log('Merch Page');
-  res.sendFile(path.join(__dirname + '/views/merch.html'));
-  // this builds localhost:3000/views/merch.html
+  //res.sendFile(path.join(__dirname + '/views/merch.html'));
+
+  res.render('merch', {title: "Merch"});
 })
 
 
 app.listen(port,  () => {
   console.log(`Server running at ${port}/`);
+  // this builds localhost:3000/views/merch.html
 });
